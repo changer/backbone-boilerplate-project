51cdf538606cd75ee859c9616d7d6b3c1c8a0f94
Ruben Stolk <ruben.stolk@changer.nl>
2013-01-20 19:46:38 +0530

    Add more demo code

--- app/modules/home.js
+++ app/modules/home.js
@@ -5,11 +5,34 @@ define([
 
   var Home = Backbone.Module();
 
+  Home.Collection = Backbone.Collection.extend({
+    url: function () {
+      return '/assets/items.json';
+    }
+  });
+
   Home.Views.Home = Backbone.View.extend({
     template: 'home',
+    events: {
+      click: 'click'
+    },
+    click: function() {
+      new Home.Collection().on('reset', function(collection) {
+        var view = new Home.Views.Items({
+          collection: collection
+        });
+        return this.setViews({ '.items': view }) && view.render();
+      }, this).fetch();
+    }
+  });
+
+  Home.Views.Items = Backbone.View.extend({
+    tagName: 'ul',
     beforeRender: function() {
-      var items = [ {name: 'a'}, {name: 'b'}, {name: 'c'}, {name: 'd'}, {name: 'e'} ];
-      _(items).each(function(item) {
+      this.insertView(new Home.Views.Item({
+        model: new Backbone.Model({ name: 'test' })
+      }));
+      this.collection.each(function(item) {
         this.insertView(new Home.Views.Item({
           model: item
         }));
